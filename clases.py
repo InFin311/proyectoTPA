d18a91543fee8b803958d45fca4eb14ceb4dbfdf
vicenteJavierveloso <vicentejavier.veloso@alumnos.ulagos.cl>
2023-07-05 18:35:48 -0400

    Actualizada clase Comanda

--- clases.py
+++ clases.py
@@ -34,9 +34,63 @@ class Mesa:
 
 
 class Comanda:
-    def __init__(self):
-        pass
+    def __init__(self,garzon: str):
+        self.garzon = garzon
+        self.hora = datetime.now().strftime("%H:%M")
+        self.pedidos = []
+        #estados 0 = en espera ; 1 = preparando; 2 = cocinando; 3 = terminado
+        self.estado = 0
 
+    def pedido_vacio(self):
+        if len(self.pedidos) == 0:
+            return True
+        else:
+            return False
+    def agregar_pedidos(self,pedido:str):
+        self.pedidos.append(pedido)
+
+    def eliminar_pedidos(self,pedido:str):
+        if self.pedido_vacio == True:
+            #Retorno false porque no puedo eliminar un pedido en una lista vacia
+            return False
+        else:
+            try:
+                self.pedido_vacio.remove(pedido)
+                return True
+            except ValueError:
+                return False
+    
+    def cambiar_estado(self,nuevo_estado:int):
+        self.estado = nuevo_estado
+        return True
+    
+    def get_estado(self):
+        if self.estado == 0:
+            return "En espera"
+        
+        elif self.estado == 1:
+            return "Preparando"
+        
+        elif self.estado == 2:
+            return "Cocinando"
+        
+        elif self.estado == 3:
+            return "Terminado"
+
+    def get_garzon(self):
+        return self.garzon
+    
+    def get_hora(self):
+        return self.hora
+    
+    def get_pedidos(self):
+        if self.pedido_vacio == True:
+            return "El pedido esta vacio"
+        else:
+            aux = str()
+            for i in self.pedidos:
+                aux.append(f"{i}\n")
+            return aux
 
 class Plato:
     def __init__(self):
